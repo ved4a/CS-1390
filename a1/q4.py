110b66f9cc57186638cb04ac6a497cea3a674e68
Vedaa Anand <work.vedaa@gmail.com>
2024-09-14 14:41:10 +0530

    feat: Extract columns for regression

--- a1/q4.py
+++ a1/q4.py
@@ -8,5 +8,6 @@ import openpyxl
 file_path = 'a1/real_estate_data.xlsx'
 df = pd.read_excel(file_path)
 
-# verify what the data looks like
-print(df.head())
+# Extract relevant columns
+features = df[['X2 House Age', 'X3 Distance to the Nearest MRT Station', 'X4 Number of Convenience Stores']]
+target = df['Y House Price of Unit Area']
