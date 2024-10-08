7dbba9a88ec33745dafc29e2d9a5b91c8c07dd70
Vedaa Anand <work.vedaa@gmail.com>
2024-10-08 15:29:53 +0530

    feat: Split data into training and test

--- a2/q2.py
+++ a2/q2.py
@@ -3,6 +3,7 @@ from ucimlrepo import fetch_ucirepo
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
   
 # Fetch dataset 
 iris = fetch_ucirepo(id=53) 
@@ -37,3 +38,6 @@ print("\nMissing values in targets:\n", missing_Y) # none
 # Implementing feature scaling using min-max scaling
 scaler = MinMaxScaler()
 scaled_X = scaler.fit_transform(X)
+
+# Split data into training and test sets
+train, test = train_test_split(iris, test_size=0.3)
