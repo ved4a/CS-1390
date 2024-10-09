5c6b272ead7a594cdbb15c2c5546aee6c4daacca
Vedaa Anand <work.vedaa@gmail.com>
2024-10-09 15:49:07 +0530

    feat: Import matplotlib

--- a2/q5.py
+++ a2/q5.py
@@ -4,6 +4,8 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression  
+import matplotlib.pyplot as plt
+
 # fetch dataset 
 real_estate_valuation = fetch_ucirepo(id=477) 
   
