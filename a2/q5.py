bf688c8241a57e0f271447aed4e9d19e418230b7
Vedaa Anand <work.vedaa@gmail.com>
2024-10-09 13:46:58 +0530

    feat: Select relevant features

--- a2/q5.py
+++ a2/q5.py
@@ -8,8 +8,10 @@ from sklearn.model_selection import train_test_split
 real_estate_valuation = fetch_ucirepo(id=477) 
   
 # data (as pandas dataframes) 
-X = real_estate_valuation.data.features 
-y = real_estate_valuation.data.targets 
+X_all = real_estate_valuation.data.features
+
+X = X_all[['X2 house age', 'X3 distance to the nearest MRT station', 'X4 number of convenience stores', 'X5 latitude', 'X6 longitude']]
+y = real_estate_valuation.data.targets
 
 # split into 3 training / test sets
 X_train_1, X_test_1, y_train_1, y_test_1 = train_test_split(X, y, test_size=0.2, random_state=42)
