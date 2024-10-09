4b0b80b11a02d13291fa54392b44d758dba49526
Vedaa Anand <work.vedaa@gmail.com>
2024-10-09 11:19:01 +0530

    feat: Store x,y,result in vars

--- a2/q3.py
+++ a2/q3.py
@@ -7,3 +7,7 @@ import openpyxl
 # Load dataset
 file_path = 'perceptron_assignment.csv'
 df = pd.read_csv(file_path)
+
+# Store columns
+X_train = df[['x', 'y']].values
+y_train = df['result'].values
