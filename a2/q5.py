cc0b77604883a92ca65c379a8787c45df7970945
Vedaa Anand <work.vedaa@gmail.com>
2024-10-09 15:00:23 +0530

    feat: Reset indices after train/test split

--- a2/q5.py
+++ a2/q5.py
@@ -17,3 +17,12 @@ y = real_estate_valuation.data.targets
 X_train_1, X_test_1, y_train_1, y_test_1 = train_test_split(X, y, test_size=0.2, random_state=42)
 X_train_2, X_test_2, y_train_2, y_test_2 = train_test_split(X, y, test_size=0.5, random_state=42)
 X_train_3, X_test_3, y_train_3, y_test_3 = train_test_split(X, y, test_size=0.9, random_state=42)
+
+# reset indices
+y_train_1 = y_train_1.reset_index(drop=True)
+y_train_2 = y_train_2.reset_index(drop=True)
+y_train_3 = y_train_3.reset_index(drop=True)
+
+y_test_1 = y_test_1.reset_index(drop=True)
+y_test_2 = y_test_2.reset_index(drop=True)
+y_test_3 = y_test_3.reset_index(drop=True)
