fae1449f7cb80ab913b0af59367e89e6aa273310
Vedaa Anand <work.vedaa@gmail.com>
2024-10-09 13:35:42 +0530

    feat: Import relevant modules

--- a2/q5.py
+++ a2/q5.py
@@ -1,4 +1,8 @@
-from ucimlrepo import fetch_ucirepo 
+from ucimlrepo import fetch_ucirepo
+
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
   
 # fetch dataset 
 real_estate_valuation = fetch_ucirepo(id=477) 
