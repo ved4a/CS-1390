5c4b2563a1e628a93b47ed047ad39ed652a556fa
Vedaa Anand <work.vedaa@gmail.com>
2024-11-12 18:52:27 +0530

    feat: plot points by class

--- a3/q6.py
+++ a3/q6.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # PART A
 
@@ -15,3 +16,15 @@ classes = (x1**2 > x2**2).astype(int)
 
 # make dataset into pandas df
 dataset = pd.DataFrame({'x1': x1, 'x2': x2, 'class': classes})
+
+# PART B
+
+plt.figure(figsize=(8, 6))
+plt.scatter(x1[classes == 0], x2[classes == 0], color='blue', label='Class 0', alpha=0.6)
+plt.scatter(x1[classes == 1], x2[classes == 1], color='red', label='Class 1', alpha=0.6)
+plt.xlabel('$x_1$')
+plt.ylabel('$x_2$')
+plt.title('Data Points')
+plt.legend()
+plt.grid(True)
+plt.show()
